7e66625384cb750bffa5bb47ff3708c31dc24faf
IrishMikey <michael.geraghty.adeje@gmail.com>
2022-02-02 16:41:24 +0000

    Darkmode, side menu

--- js/script.js
+++ js/script.js
@@ -1,27 +1,53 @@
+// Enable/Disable dark mode with slider state
+
 var slider = document.getElementById("slider");
 var sliderContainer = document.getElementById("sliderContainer");
+
 var main = document.getElementsByTagName("main")[0];
 var body = document.getElementsByTagName("body")[0];
-var sliderState = "light";
 
+
+var sliderState = "light";
 function setDarkMode() {
     sliderState = "dark";
-    slider.style.left = "25px";
-    main.style.backgroundImage = "url('../img/bg-dark.png')";
-    body.style.color = "white";
+    slider.setAttribute("class", 'darkModeOn');
+    body.setAttribute("class", "bodyDark");
 }
 function setLightMode() {
     sliderState = "light";
-    slider.style.left = "";
-    main.style.backgroundImage = "url('../img/bg-light.png')";
-    body.style.color = "black";
+    slider.setAttribute("class", 'lightModeOn');
+    body.removeAttribute("class", "bodyDark");
 }
 
 sliderContainer.addEventListener("click", () => {
-    if (sliderState == "light") {
+    if (body.classList.contains('bodyDark')) {
+        setLightMode();
+    } else
         setDarkMode();
-    } else if (sliderState == "dark") {
-        setLightMode()
-    }
 
-})
+});
+
+
+// Enable/Disable button with checkbox state
+
+var chekBox = document.getElementById("confirmarCheck");
+var enterBtn = document.getElementById("enterBtn");
+chekBox.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        enterBtn.disabled = false;
+    } else {
+        enterBtn.disabled = true;
+    }
+    console.log(enterBtn);
+});
+var aside = document.getElementsByTagName("aside")[0];
+
+var dMenu = document.getElementById("dMenu");
+dMenu.addEventListener("click", () => {
+    if (aside.classList.contains('closeMenu')) {
+        aside.setAttribute("class", 'openMenu');
+    } else
+        aside.setAttribute("class", 'closeMenu');
+
+});
+
